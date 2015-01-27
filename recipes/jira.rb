75bfcba990e4c45b956af714f419ac35ca7c1775
Boye Holden <boye.holden@hist.no>
2015-01-27 10:23:26 +0100

    Use Oracle java

--- recipes/jira.rb
+++ recipes/jira.rb
@@ -17,6 +17,13 @@
 # limitations under the License.
 #
 
+node.set['java'] = {
+  'install_flavor' => 'oracle',
+  'jdk_version' => '7',
+  'oracle' => {
+    'accept_oracle_download_terms' => true
+  }
+}
 include_recipe 'java::default'
 
 group 'jira' do
